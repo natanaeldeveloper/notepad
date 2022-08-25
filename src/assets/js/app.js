748687397592dcbdc1682f166fc48931973f6c7b
Natanael Oliveira <natanaeloliveiramartinsbr@gmail.com>
2022-08-25 17:39:04 -0300

    adicionado primeiro modelo da etapa de realizar anotações

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,5 +1,9 @@
-import { handleToggleEvents } from './events';
+import { handleToggleEvents, resizeTextfield } from './events';
 
 $(document).ready(function() {
     handleToggleEvents()    
+
+    var el = document.getElementById('content')
+    // resizeTextfield(el);
+
 })
--- src/assets/js/events.js
+++ src/assets/js/events.js
@@ -1,7 +1,7 @@
 export const handleToggleEvents = () => {
 
-    const elements =  [...$('*[data-toggle="toggle"]')]
-    
+    const elements = [...$('*[data-toggle="toggle"]')]
+
     elements.forEach(element => {
         $(element).click(function (event) {
             event.preventDefault()
@@ -10,25 +10,63 @@ export const handleToggleEvents = () => {
                 let targetValue = $(this).data('target')
                 let targetElement = $(targetValue === undefined ? this : targetValue)
 
-                if(targetElement.length < 1) {
+                if (targetElement.length < 1) {
                     throw `Elemento de referência "${targetValue}" não encontrado na DOM;`
                 }
 
                 $(targetElement).toggleClass('--active')
 
                 /* next default */
-                if(nodeName === 'LABEL') {
+                if (nodeName === 'LABEL') {
                     let attrForValue = $(this).attr('for')
-                    let inputElement = attrForValue === undefined ? 
+                    let inputElement = attrForValue === undefined ?
                         $(this).find('input') : $(`#${attrForValue}`)
                     inputElement.prop('checked', (i, val) => {
                         return !val
                     })
                 }
-    
-            } catch(e) {
+
+            } catch (e) {
                 console.error(e, 'Evento disparado pelo elemento: ', $(element))
             }
         })
     })
+}
+
+function addEvent(type, el, callback)
+{
+ if (window.addEventListener) {
+     el.addEventListener(type, callback, false);
+ } else if (window.attachEvent) {
+     el.attachEvent("on" + type, callback);
+ } else {
+     el["on" + type] = callback;
+ }
+}
+
+export const resizeTextfield = (el) =>
+{
+ var timer;
+
+ function trigger() {
+     if (!el) { return; }
+
+     el.style.height = "auto";
+
+     var height = el.scrollHeight;
+
+     el.style.height = height + "px";
+ }
+
+ function exec() {
+     if (timer) {
+         clearTimeout(timer);
+     }
+
+     timer = setTimeout(trigger, 10);
+     console.log('aaa')
+ }
+
+ addEvent("keyup", el, exec);
+ addEvent("input", el, exec);
 }
