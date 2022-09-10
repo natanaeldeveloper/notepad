88c30bbd03dbe17c8b59cabbd36b409a731f6631
natanael-oliveira <natanaeloliveiramartinsbr@gmail.com>
2022-09-10 17:48:56 -0300

    adicionado função de authenticação

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,14 +2,26 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StoreUserRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class AuthController extends Controller
 {
 
-    public function register(StoreUserRequest $request)
+    public function login(Request $request)
     {
-        dd($request->validated());
+        $credentials = $request->only('email', 'password');
+
+        if(!Auth::attempt($credentials, $request->remember)) {
+            throw (new HttpException(401, 'Unauthorized.'));
+        }
+
+        return response()->json([
+            'status' => 200,
+            'title'  => 'Successiful authentication.',
+            'user'   => Auth::user()
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,11 +16,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
-// Authentication
 Route::prefix('auth')->group(function() {
-    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
 });
+
+Route::apiResource('users', UserController::class)->names('user');
